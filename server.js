4995df07ff54cb9e70af0102d2f323bd8a0177d8
Anurag Gupta <aguptaking@gmail.com>
2020-10-10 17:29:52 +0530

    pwa#6

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
+    res.redirect('https://' + req.headers.host + req.url);
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
